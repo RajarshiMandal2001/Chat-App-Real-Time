3c65a724ca5ff7ef93f213ce28d9410a1fc41c4a
RajarshiMandal2001 <76610610+RajarshiMandal2001@users.noreply.github.com>
2025-03-05 13:54:59 +0530

    Update socket.js with online URL

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors:{
-        origin:['http://localhost:3000'],
+        origin:['http://localhost:3000', 'https://chat-app-real-time-6t4b.onrender.com'],
         methods:['GET', 'POST'],
     },
 });
